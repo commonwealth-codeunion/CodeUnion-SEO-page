c357cc0d3f7d966830a9310c9720218683aed576
Nurlan Zhumakhan <nurlanzumahan@gmail.com>
2020-11-10 16:29:21 +0600

    feat: add services section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-$(function () {
-
-});
+document.addEventListener('DOMContentLoaded', () => {
+    var mySwiper = new Swiper('.services__slider', {
+        // effect: 'fade',
+        speed: 400,
+        loop: true,
+        spaceBetween: 100,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})
