068557a346ac9b506f7d67e1d80b626d0004b833
Nurlan Zhumakhan <nurlanzumahan@gmail.com>
2020-11-10 17:22:12 +0600

    feat: add trust section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var mySwiper = new Swiper('.services__slider', {
+    const trust = document.querySelector('.trust');
+    const header = document.querySelector('.header');
+    const footer = document.querySelector('.footer');
+    const trustScrollHeight = trust.offsetTop;
+    console.log(trustScrollHeight);
+    document.addEventListener('scroll', ()=>{
+        if(pageYOffset >= trustScrollHeight){
+            header.style.color = "#000";
+            footer.style.color = "#000";
+        }else{
+            header.style.color = "";
+            footer.style.color = "";
+        }
+    })
+    let servicesSwiper = new Swiper('.services__slider', {
         // effect: 'fade',
         speed: 400,
         loop: true,
@@ -12,5 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+    });
+    let trustSwiper = new Swiper('.trust__slider', {
+        // effect: 'fade',
+        slidesPerView: 3,
+        speed: 400,
+        loop: true,
+        spaceBetween: 108,
     });
 })
