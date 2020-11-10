83120464ab5ae55f80df178d6c827c9cf87ed3c0
Nurlan Zhumakhan <nurlanzumahan@gmail.com>
2020-11-10 20:42:57 +0600

    feat: add popup

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,44 @@ document.addEventListener('DOMContentLoaded', () => {
     new fullpage('#fullpage', {
         licenseKey: 'YOUR KEY HERE',
     });
+    const overflow = document.querySelector('.overflow');
+    const popupBtn = document.querySelectorAll('.popup-btn');
+    const popup = document.querySelector('.popup');
+    const popupClose = document.querySelector('.popup__close');
     const mainButton = document.querySelector('.main-button');
     const price = document.querySelector('.price');
     const header = document.querySelector('.header');
     const footer = document.querySelector('.footer');
     const priceScrollHeight = price.offsetTop;
-    console.log(priceScrollHeight);
-    document.addEventListener('scroll', () => {
-        console.log(pageYOffset)
-        if (pageYOffset >= priceScrollHeight) {
-            mainButton.classList.add("dark");
-            header.classList.add("dark");
-            footer.classList.add("dark");
-        } else {
-            mainButton.classList.remove("dark");
-            header.classList.remove("dark");
-            footer.classList.remove("dark");
-        }
+    // document.addEventListener('scroll', () => {
+    //     console.log(pageYOffset)
+    //     if (pageYOffset >= priceScrollHeight) {
+    //         mainButton.classList.add("dark");
+    //         header.classList.add("dark");
+    //         footer.classList.add("dark");
+    //     } else {
+    //         mainButton.classList.remove("dark");
+    //         header.classList.remove("dark");
+    //         footer.classList.remove("dark");
+    //     }
+    // });
+    popupBtn.forEach(elem => {
+        elem.addEventListener('click', () => {
+            popup.classList.add('active');
+            overflow.classList.add('active');
+            document.body.classList.add('lock');
+        });
     })
+    popupClose.addEventListener('click', () => {
+        popup.classList.remove('active');
+        overflow.classList.remove('active');
+        document.body.classList.remove('lock');
+    });
+    overflow.addEventListener('click', () => {
+        popup.classList.remove('active');
+        overflow.classList.remove('active');
+        document.body.classList.remove('lock');
+    });
     let servicesSwiper = new Swiper('.services__slider', {
         // effect: 'fade',
         speed: 400,
