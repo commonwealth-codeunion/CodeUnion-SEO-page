6d8b491cedb13ab322c881e1ceca2c9c383ffc5b
Nurlan Zhumakhan <nurlanzumahan@gmail.com>
2020-11-12 16:37:17 +0600

    fix: horizontal scroll works everywhere now

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         licenseKey: 'YOUR KEY HERE',
         navigation: true,
         anchors: ['promotion', 'why', 'services', 'price', 'trust'],
-        normalScrollElements: ['.price__items-wrapper', '.trust__slider']
+        normalScrollElements: '.price__items-wrapper, .trust__slider',
     });
     let servicesSwiper = new Swiper('.services__slider', {
         speed: 400,
